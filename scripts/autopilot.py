aa2debe88f85596867fcb2f97ee3c56f7eb819da
Mark Broerkens <broerkens@gmail.com>
2019-07-23 07:50:15 -0700

    initial code for keras training

---
+++ scripts/autopilot.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+import time
+
+import rospy
+from ackermann_msgs.msg import AckermannDriveStamped
+from sensor_msgs.msg import Image
+
+from keras.models import load_model
+from keras_preprocessing.image.utils import load_img, img_to_array
+
+from PIL import Image as pil_image
+from io import BytesIO
+import numpy as np
+
+class ROSPackage_Autopilot:
+    def __init__(self):
+        rospy.init_node('autopilotr')
+        rospy.loginfo("AI driver init. Loading neural network...")
+        self.model = load_model('/tmp/best_model.h5')
+        rospy.loginfo("Neural network loaded, starting service.")
+
+        self.value_to_publish = AckermannDriveStamped()
+        self.value_to_publish.drive.speed = 1.0
+        self.value_to_publish.drive.steering_angle = 0.0
+        self.autopilot_publisher = rospy.Publisher(
+            'ackermann_cmd_mux/input/autopilot', AckermannDriveStamped, queue_size=10)
+        rospy.Subscriber('camera/color/image_raw', Image, self.receive_image)
+
+    def start(self):
+        rospy.loginfo("Autopilot start")
+        rospy.spin()
+        #rate = rospy.Rate(10)
+        #while not rospy.is_shutdown():
+            #self.ai_driver_publisher.publish(self.value_to_publish)
+            #rate.sleep()
+
+    def image_to_array(self, img):
+        return img_to_array(load_img(BytesIO(img), target_size=(32,32)))
+
+    def receive_image(self, img):
+        rospy.loginfo("img" + type(img).__name__)
+        rospy.loginfo("img" + str(len(img.data)))
+        #image = self.image_to_array(img.data)
+        #image=np.expand_dims(image,0)
+        #prediction = self.model.predict(image)
+        #rospy.loginfo(prediction[0])
+        #value = (np.argmax(prediction[0]) - 2) / 2
+        #rospy.loginfo(str(value))
+        #self.value_to_publish.drive.steering_angle = value
+        #self.ai_driver_publisher.publish(self.value_to_publish)
+
+import sys
+if __name__ == '__main__':
+    package = ROSPackage_Autopilot()
+    try:
+        package.start()
+    except rospy.ROSInterruptException:
+        pass
---
+++ tools/train.py
@@ -0,0 +1,236 @@
+"""Train the model"""
+
+import argparse
+import logging
+import os
+import random
+
+import tensorflow as tf
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from model.utils import Params
+from model.utils import set_logger
+from model.utils import save_dict_to_json
+
+from random import getrandbits
+
+from tensorflow.python import keras
+from tensorflow.python.keras.layers import Input, Dense
+from tensorflow.python.keras.models import Model, Sequential
+from tensorflow.python.keras.layers import Convolution2D, MaxPooling2D, Reshape, BatchNormalization
+from tensorflow.python.keras.layers import Activation, Dropout, Flatten, Cropping2D, Lambda
+from tensorflow.python.keras.layers.merge import concatenate
+from tensorflow.python.keras.layers import LSTM
+from tensorflow.python.keras.layers.wrappers import TimeDistributed as TD
+from tensorflow.python.keras.layers import Conv3D, MaxPooling3D, Cropping3D, Conv2DTranspose
+
+
+def _parse_function(filename, labels, size):
+    """Obtain the image from the filename (for both training and validation).
+
+    The following operations are applied:
+        - Decode the image from jpeg format
+        - Convert to float and to range [0, 1]
+    """
+    image_string = tf.io.read_file(filename)
+
+    # Don't use tf.image.decode_image, or the output shape will be undefined
+    image_decoded = tf.image.decode_jpeg(image_string, channels=3)
+
+    # This will convert to float values in [0, 1]
+    image = tf.image.convert_image_dtype(image_decoded, tf.float32)
+
+    resized_image = tf.image.resize(image, [size, size])
+    resized_image = tf.image.per_image_standardization(resized_image)
+
+    return resized_image, labels
+
+
+def train_preprocess(image, labels):
+    """Image preprocessing for training.
+
+    Apply the following operations:
+        - Horizontally flip the image with probability 1/2
+        - Apply random brightness and saturation
+    """
+    if not getrandbits(1):
+        image = tf.image.flip_left_right(image)
+        # flip steering_angle
+        labels = (labels[0] * -1,) + labels[1:]
+    #image = tf.image.random_brightness(image, max_delta=32.0 / 255.0)
+    #image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+
+    # Make sure the image is still in [0, 1]
+    #image = tf.clip_by_value(image, 0.0, 1.0)
+
+    return image, labels
+
+
+def input_fn(is_training, filenames, labels, params):
+    """Input function for the SIGNS dataset.
+
+    The filenames have format "{label}_IMG_{id}.jpg".
+    For instance: "data_dir/2_IMG_4584.jpg".
+
+    Args:
+        is_training: (bool) whether to use the train or test pipeline.
+                     At training, we shuffle the data and have multiple epochs
+        filenames: (list) filenames of the images, as ["data_dir/{label}_IMG_{id}.jpg"...]
+        labels: (list) corresponding list of labels
+        params: (Params) contains hyperparameters of the model (ex: `params.num_epochs`)
+    """
+    num_samples = len(filenames)
+    assert len(filenames) == len(labels), "Filenames and labels should have same length"
+
+    # Create a Dataset serving batches of images and labels
+    # We don't repeat for multiple epochs because we always train and evaluate for one epoch
+    parse_fn = lambda f, l: _parse_function(f, l, params.image_size)
+    train_fn = lambda f, l: train_preprocess(f, l)
+
+    if is_training:
+        dataset = (tf.data.Dataset.from_tensor_slices((filenames, (labels[['steering_angle']], labels[['speed']])))
+            .shuffle(num_samples)  # whole dataset into the buffer ensures good shuffling
+            .map(parse_fn, num_parallel_calls=params.num_parallel_calls)
+            .map(train_fn, num_parallel_calls=params.num_parallel_calls)
+            .batch(params.batch_size)
+            .repeat()
+            .prefetch(1)  # make sure you always have one batch ready to serve
+        )
+    else:
+        dataset = (tf.data.Dataset.from_tensor_slices((filenames, (labels[['steering_angle']], labels[['speed']])))
+            .map(parse_fn)
+            .repeat()
+            .batch(params.batch_size)
+            .prefetch(1)  # make sure you always have one batch ready to serve
+        )
+
+    return dataset
+
+
+def training_pipeline(train_filenames, train_labels, eval_filenames, eval_labels, model_dir, params):
+    training_set = input_fn(True, train_filenames, train_labels, params)
+    testing_set  = input_fn(False, eval_filenames, eval_labels, params)
+
+    try:
+        os.makedirs(model_dir)
+    except OSError:
+        if not os.path.isdir(model_dir):
+            raise
+
+    model_file = os.path.join(model_dir, "model.h5")
+
+    # #############
+    # Train Model
+    # #############
+    model = default_n_linear(num_outputs=2)  # your keras model here
+    model.compile(optimizer='adam',
+                  loss=['mean_squared_error', 'mean_squared_error'] #,
+                  #metrics=['mean_squared_error','mean_squared_error']
+                  )
+
+    early_stop = keras.callbacks.EarlyStopping(monitor='val_loss',
+                                               min_delta=params.min_delta,
+                                               patience=params.early_stop_patience,
+                                               verbose=1,
+                                               mode='auto')
+    save_best = keras.callbacks.ModelCheckpoint(model_file,
+                                                monitor='val_loss',
+                                                verbose=1,
+                                                save_best_only=True,
+                                                save_weights_only=False,
+                                                mode='auto')
+
+    model.fit(
+        training_set,
+        steps_per_epoch=len(train_filenames) // params.batch_size,
+        epochs=params.num_epochs,
+        validation_data=testing_set,
+        validation_steps=len(eval_filenames) // params.batch_size,
+        verbose = 1,
+        callbacks=[save_best, early_stop])
+
+
+def adjust_input_shape(input_shape, roi_crop):
+    height = input_shape[0]
+    new_height = height - roi_crop[0] - roi_crop[1]
+    return (new_height, input_shape[1], input_shape[2])
+
+
+def default_n_linear(num_outputs=2, input_shape=(64, 64, 3), roi_crop=(0, 0)):
+    drop = 0.1
+
+    # we now expect that cropping done elsewhere. we will adjust our expeected image size here:
+    input_shape = adjust_input_shape(input_shape, roi_crop)
+
+    img_in = Input(shape=input_shape, name='img_in')
+    x = img_in
+    x = Convolution2D(24, (5, 5), strides=(2, 2), activation='relu', name="conv2d_1")(x)
+    x = Dropout(drop)(x)
+    x = Convolution2D(32, (5, 5), strides=(2, 2), activation='relu', name="conv2d_2")(x)
+    x = Dropout(drop)(x)
+    x = Convolution2D(64, (5, 5), strides=(2, 2), activation='relu', name="conv2d_3")(x)
+    x = Dropout(drop)(x)
+    x = Convolution2D(64, (3, 3), strides=(1, 1), activation='relu', name="conv2d_4")(x)
+    x = Dropout(drop)(x)
+    x = Convolution2D(64, (3, 3), strides=(1, 1), activation='relu', name="conv2d_5")(x)
+    x = Dropout(drop)(x)
+
+    x = Flatten(name='flattened')(x)
+    x = Dense(100, activation='relu')(x)
+    x = Dropout(drop)(x)
+    x = Dense(50, activation='relu')(x)
+    x = Dropout(drop)(x)
+
+    outputs = []
+
+    for i in range(num_outputs):
+        outputs.append(Dense(1, activation='linear', name='n_outputs' + str(i))(x))
+
+    model = Model(inputs=[img_in], outputs=outputs)
+
+    return model
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_dir', default='experiments/learning_rate',
+                        help="Experiment directory containing params.json")
+    parser.add_argument('--data_dir', default='/data/cleaned',
+                        help="Directory containing the dataset")
+    parser.add_argument('--restore_from', default=None,
+                        help="Optional, directory or file containing weights to reload before training")
+
+    # Set the random seed for the whole graph for reproductible experiments
+    tf.set_random_seed(230)
+
+    # Load the parameters from json file
+    args = parser.parse_args()
+    json_path = os.path.join(args.model_dir, 'params.json')
+    assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
+    params = Params(json_path)
+
+    # Set the logger
+    set_logger(os.path.join(args.model_dir, 'train.log'))
+
+    # Create the input data pipeline
+    logging.info("Creating the datasets...")
+    data_dir = args.data_dir
+
+    # find the CSV file(s)
+    dataframe_csv = os.path.join(data_dir, 'dataset.csv')
+    dataframe = pd.read_csv(dataframe_csv)
+    filenames = [os.path.join(data_dir, filename) for filename in dataframe.filename]
+
+    labels = dataframe[['steering_angle', 'speed']]
+
+    print(labels)
+
+    train_filenames, eval_filenames, train_labels, eval_labels = train_test_split(filenames, labels, test_size=0.2)
+
+    training_pipeline(train_filenames, train_labels, eval_filenames, eval_labels, args.model_dir, params)
+
+
+if __name__ == '__main__':
+    main()
